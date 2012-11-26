806612c90c14e15b605d3812e2e91ff3f4e856fe
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-11-26 13:00:34 +0530

    Don't wrap lists and tables in Hashes. Use native (nested) Arrays.

--- lib/calvin/transform.rb
+++ lib/calvin/transform.rb
@@ -3,6 +3,8 @@ module Calvin
     rule(integer:  simple(:x))  { x.to_i }
     rule(float:    simple(:x))  { x.to_f }
     rule(atom:     simple(:x))  { x }
+    rule(list:     subtree(:x)) { x }
+    rule(table:    subtree(:x)) { x }
     rule(noun:     subtree(:x)) { x }
     rule(sentence: subtree(:x)) { x }
   end
--- spec/ast_spec.rb
+++ spec/ast_spec.rb
@@ -17,14 +17,14 @@ describe Calvin::AST do
 
   describe "List" do
     it "should parse lists" do
-      ast1("1 2").should eq list: [1, 2]
-      ast1("12 2.3").should eq list: [12, 2.3]
+      ast1("1 2").should eq [1, 2]
+      ast1("12 2.3").should eq [12, 2.3]
     end
   end
 
   describe "Table" do
     it "should parse tables" do
-      ast1("[1 1, 2 2]").should eq table: [{list: [1, 1]}, {list: [2, 2]}]
+      ast1("[1 1, 2 2]").should eq [[1, 1], [2, 2]]
     end
   end
 
@@ -45,12 +45,11 @@ describe Calvin::AST do
 
   describe "Sentences" do
     it "should parse double dyads/monads sentence" do
-      ast1("1 2 + 3 4").should eq dyad: { left: { list: [1, 2] },
-                                          symbol: "+", right: { list: [3, 4] } }
-      ast1("1 2 + 3").should eq dyad: { left: { list: [1, 2] },
-                                        symbol: "+", right: 3 }
-      ast1("[1 2, 3 4] + 1").should eq dyad: { left: { table: [ {list: [1, 2] },
-                                    { list: [3, 4] }] }, symbol: "+", right: 1 }
+      ast1("1 2 + 3 4").should eq dyad: { left: [1, 2], symbol: "+",
+                                          right: [3, 4] }
+      ast1("1 2 + 3").should eq dyad: { left: [1, 2], symbol: "+", right: 3 }
+      ast1("[1 2, 3 4] + 1").should eq dyad: { left: [[1, 2], [3, 4]],
+                                               symbol: "+", right: 1 }
     end
   end
 end
