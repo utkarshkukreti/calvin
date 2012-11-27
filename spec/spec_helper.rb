97ca8df7793d69e73b527acd52f1fe3f076c683a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-11-27 20:45:48 +0530

    Add simplecov, for test coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,18 @@
 require "bundler/setup"
+require "simplecov"
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/features/'
+  add_filter '/spec/'
+  add_filter '/autotest/'
+
+  add_group 'Binaries', '/bin/'
+  add_group 'Libraries', 'lib/'
+  add_group 'Extensions', '/ext/'
+  add_group 'Vendor Libraries', '/vendor/'
+end
+
 require "calvin"
 
 RSpec.configure do |config|
